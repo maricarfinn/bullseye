7e7e1a56d9cc6373a89a11baf1cf072f80667762
Maricar Finn <maricar.finn@gmail.com>
2020-06-04 10:15:37 +0200

    Latest updates 04/06/2020

--- Bullseye/ContentView.swift
+++ Bullseye/ContentView.swift
@@ -10,31 +10,120 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var alertIsVisible: Bool = false
+    @State var alertIsVisible = false
+    @State var sliderValue = 50.0
+    @State var target = Int.random(in: 1...100)
+    
+    @State var score = 0
+    @State var currentRound = 1
     
     var body: some View {
         VStack {
+            Spacer()
             // Target row
-            Text("Welcome to my first app")
-                .fontWeight(.semibold)
-                .foregroundColor(Color.green)
-            
+            HStack {
+                Text("Put the bullseye as close as you can to:")
+                    .foregroundColor(Color.white)
+                Text ("\(target)")
+            }
+            Spacer()
             // Slider row
-            
+            HStack {
+                Text ("1")
+                Slider(value: $sliderValue, in: 1...100)
+                Text ("100")
+            }
+            Spacer()
             //Button row
             Button(action: {
-                print("Button pressed")
                 self.alertIsVisible = true
+                self.score = self.score + self.pointsForCurrentRound()
             }) {
                 Text(/*@START_MENU_TOKEN@*/"Hit me"/*@END_MENU_TOKEN@*/)
             }
             .alert(isPresented: $alertIsVisible) { () ->
                 Alert in
-                return Alert(title: Text("Hello there!"), message: Text("This is my first pop-up."), dismissButton: .default(Text("Awesome!")))
+                return Alert(title: Text(alertTitle()), message: Text("The slider's value is \(sliderValueRounded()).\n" +
+                
+                    "You scored \(pointsForCurrentRound()) points this round"), dismissButton: .default(Text("Awesome!")) {
+                        self.target = Int.random(in: 1...100)
+                        self.currentRound = self.currentRound + 1
+                    }
+                
+                )
+                
             }
-            
+            Spacer()
             // Score row
+            
+            HStack {
+                Button(action: {
+                    self.resetGame()
+                }) {
+                    Text("Start over")
+                }
+                Spacer()
+                Text ("Score:")
+                Text("\(Int(score))")
+                Spacer()
+                Text ("Round:")
+                Text("\(Int(currentRound))")
+                Spacer()
+                Button(action: /*@START_MENU_TOKEN@*/{}/*@END_MENU_TOKEN@*/) {
+                    Text("Info")
+                }
+            }
+            .padding(.bottom, 20)
         }
+        .background(Image("Background"), alignment: .center)
+    }
+    
+    func sliderValueRounded() -> Int {
+        Int(sliderValue.rounded())
+    }
+    
+    func amountOff() -> Int {
+        return abs(target - sliderValueRounded())
+    }
+    
+    func pointsForCurrentRound () -> Int {
+        
+        let maximumScore = 100
+        let difference = amountOff()
+        let bonus: Int
+        
+        if difference == 0 {
+            bonus = 100
+        } else if difference == 1 {
+            bonus = 50
+        } else {
+            bonus = 0
+        }
+        return maximumScore - difference + bonus
+    }
+    
+    func alertTitle() -> String {
+        let difference = amountOff()
+        let title: String
+        
+        if difference == 0 {
+            title = "Perfect!"
+            
+        } else if difference < 5 {
+            title = "You almost had it"
+        } else if difference <= 10 {
+            title = "Not bad"
+        } else {
+            title = "Are you even trying?"
+        }
+        return title
+    }
+    
+    func resetGame() {
+        
+        score = 0
+        currentRound = 1
+        sliderValue = 50.0
     }
 }
 
